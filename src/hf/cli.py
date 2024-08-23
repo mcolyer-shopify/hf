5c49114334a8dcecad1c312f2171fa3fc56c5b5c
Matt Colyer <matt.colyer@shopify.com>
2024-08-23 12:21:41 -0700

    Add the ability to only show open discussions

--- src/hf/cli.py
+++ src/hf/cli.py
@@ -2,11 +2,11 @@ import fire
 from huggingface_hub import HfApi
 
 class HFCLI:
-    def discussions(self, repo: str):
+    def discussions(self, repo: str, show_all: bool = False):
         api = HfApi()
-        discussions = api.get_repo_discussions(repo_id=repo)
+        discussions = api.get_repo_discussions(repo_id=repo, discussion_status=None if show_all else "open")
         for discussion in discussions:
-            print(f"Title: {discussion.title}, Created At: {discussion.created_at}")
+            print(f"#{discussion.num} Title: {discussion.title}, Created At: {discussion.created_at}")
 
 def main():
     fire.Fire(HFCLI)
