bb8adff6c639064df1351a546163a84761eecb4f
Matt Colyer <matt.colyer@shopify.com>
2024-08-23 12:17:32 -0700

    It works!

--- src/hf/cli.py
+++ src/hf/cli.py
@@ -4,7 +4,7 @@ from huggingface_hub import HfApi
 class HFCLI:
     def discussions(self, repo: str):
         api = HfApi()
-        discussions = api.list_discussions(repo_id=repo)
+        discussions = api.get_repo_discussions(repo_id=repo)
         for discussion in discussions:
             print(f"Title: {discussion.title}, Created At: {discussion.created_at}")
 
