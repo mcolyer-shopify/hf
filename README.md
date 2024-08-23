f72e0f26c0c8c5a2ea4be1a22429269bf390b4fe
Matt Colyer <matt.colyer@shopify.com>
2024-08-23 12:29:20 -0700

    Add the ability to close discussions

--- README.md
+++ README.md
@@ -2,14 +2,13 @@
 
 A CLI tool to list discussions on a Hugging Face repo.
 
-## Installation
-
-```bash
-pip install .
-```
+Today it only supports listing and closing discussions, maybe one day it will
+do more.
 
 ## Usage
 
 ```bash
-hf org/repo discussions
+HF_TOKEN=your_token_here
+uv run hf discussions list org/repo
+uv run hf discussions close org/repo #123
 ```
--- src/hf/cli.py
+++ src/hf/cli.py
@@ -1,15 +1,24 @@
 import fire
 from huggingface_hub import HfApi
 
-class HFCLI:
-    def discussions(self, repo: str, show_all: bool = False):
+class Discussions:
+    def list(self, repo: str, show_all: bool = False):
         api = HfApi()
         discussions = api.get_repo_discussions(repo_id=repo, discussion_status=None if show_all else "open")
         for discussion in discussions:
             print(f"#{discussion.num} Title: {discussion.title}, Created At: {discussion.created_at}")
 
+    def close(self, repo: str, num: int):
+        api = HfApi()
+        api.change_discussion_status(repo_id=repo, discussion_num=num, new_status="closed")
+        print(f"Discussion {num} has been closed.")
+
+class HFCLI:
+    def __init__(self):
+        self.discussions = Discussions()
+
 def main():
-    fire.Fire(HFCLI)
+    fire.Fire(HFCLI())
 
 if __name__ == "__main__":
     main()
